ff556fd41f95782e8f7508bc83402fec8f7e6db8
Nogueira <spbiel18@gmail.com>
2024-12-03 09:21:52 -0300

    primeiro commit

---
+++ src/introducao/EntradaSaida.java
@@ -0,0 +1,36 @@
+package introducao;
+
+import java.util.Scanner; //importando a scanner
+
+public class EntradaSaida {
+
+	public static void main(String[] args) {
+	// Variaveis
+	String nome;
+	float temperatura;
+	float fahrenheit;
+	
+	// Puxando o scanner
+	Scanner leia = new Scanner(System.in);
+	
+		
+	//Entrada de dados - Pedir/Inserir o nome, temperatura
+	System.out.println("Insira o seu nome: ");
+	nome = leia.nextLine();
+	
+	System.out.println("Insira a temperatura em Celsius: ");
+	temperatura = leia.nextFloat();
+	
+
+	
+	// Processamento
+	fahrenheit = temperatura * 1.8f + 32;
+			
+	// Saída de dados
+	System.out.println("Bom dia, Magnata " + nome);	// concatenação
+
+	System.out.printf("A temperatura em Fahrenheit é %.1f", fahrenheit);	
+
+	}
+
+}
